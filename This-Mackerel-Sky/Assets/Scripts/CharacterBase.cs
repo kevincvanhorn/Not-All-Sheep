a108bd30553cc89a82969ac8d91d3d0f0d9ccef9
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2017-12-20 17:58:42 -0600

    Base import.

---
+++ This-Mackerel-Sky/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MonsterLove.StateMachine; // State-Machine Package.
+
+public class CharacterBase : MonoBehaviour {
+
+
+    /* Define States */
+    public enum States {
+        Idle,
+        Jumping,
+        Falling,
+        WallRising,
+        WallFalling,
+        WallSticking,
+        Sprinting,
+        Dashing,
+        ClimbingSlope
+    }
+
+    private StateMachine<States> fsm;
+
+    public void Awake() {
+        // Initialize State Machine Engine		
+        fsm = StateMachine<States>.Initialize(this, States.Falling);
+    }
+	
+	// Update is called once per frame
+	void Update() {
+        CalcState();
+	}
+
+    void CalcState() {
+        var state = fsm.State;
+        if(state == States.Idle) {
+
+        }
+    }
+
+    void Idle_Enter() {
+
+    }
+
+    void Idle_Update() {
+        if () {
+
+        }
+    }
+
+    void Idle_Exit() {
+
+    }
+
+    void Idle_Finally() {
+        // Reset object to desired configuration
+        // For Overwrite: fsm.ChangeState(States.MyNextState, StateTransition.Overwrite);
+    }
+}
