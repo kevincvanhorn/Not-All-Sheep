7384f7ea0ed9299faa694d353adafb34047135a4
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2017-12-21 17:02:04 -0600

    In Progress - Falling State Transitions.

--- This-Mackerel-Sky/Assets/Scripts/CharacterBase.cs
+++ This-Mackerel-Sky/Assets/Scripts/CharacterBase.cs
@@ -106,22 +106,16 @@ public class CharacterBase : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        Debug.Log("MAIN - Update");
-        rigidBody.velocity = velocity;
-    }
-
-    /* Synchronous w/ Custom State fixed updates - for common resetting of attributes. */
-    void BaseFixedUpdate() {
+        Debug.Log("Main - Fixed Update");
         enterCollisionTypes.Clear();
         exitCollisionTypes.Clear();
+        rigidBody.velocity = velocity;
     }
 
     /** Called on Player collision with a new object. **/
     void BaseCollisionEnter2D(Collision2D collision) { // ~ Could convert Collision2D to Collider2D
         ContactPoint2D[] contactsIn = new ContactPoint2D[4]; // 2 when side collides (each corner) || 1 when on slope
         collision.GetContacts(contactsIn);
-
-        bool enterSet = false;
 
         /* Add new contact points to hash. */
         for (int i = 0; i < contactsIn.Length; i++) {
@@ -134,7 +128,7 @@ public class CharacterBase : MonoBehaviour {
         }
 
         /* Call Collider Enter Functions */
-        for (int i = 0; !enterSet && i < contactsIn.Length; i++) {
+        for (int i = 0; i < contactsIn.Length; i++) {
             /* If contact exists (entries are zero in larger alocated ContactPoint2D[])*/
             //print("--------------" + slopeAngle + " " + contactsIn[i].normal);
             if (contactsIn[i].normal != Vector2.zero) {
@@ -143,22 +137,18 @@ public class CharacterBase : MonoBehaviour {
                 if (contactsIn[i].normal.x == 0) { // contactsIn[i].normal.x == 0
                     if (contactsIn[i].normal.y == 1) {
                         enterCollisionTypes.Add(CollisionType.Bot);
-                        enterSet = true;
                     }
                     else if (contactsIn[i].normal.y == -1) { // contactsIn[i].normal.y == -1
                         enterCollisionTypes.Add(CollisionType.Top);
-                        enterSet = true;
                     }
                 }
                 /* Horizontal Collision */
                 else if (slopeAngle > maxAngle) { // contactsIn[i].normal.y == 0
                     if (contactsIn[i].normal.x > 0) { // contactsIn[i].normal.x == 1
                         enterCollisionTypes.Add(CollisionType.Left);
-                        enterSet = true;
                     }
                     else if (contactsIn[i].normal.x < 0) { // contactsIn[i].normal.x == -1
                         enterCollisionTypes.Add(CollisionType.Right);
-                        enterSet = true;
                     }
                 }
                 /* Slope Collision */
@@ -176,7 +166,6 @@ public class CharacterBase : MonoBehaviour {
         ContactPoint2D[] contactsRB = new ContactPoint2D[4]; // 2 when side collides (each corner) || 1 when on slope
         rigidBody.GetContacts(contactsRB);
 
-        bool exitSet = false;
         float slopeAngleExit = 0;
 
         /* Make a hash with the current normals touching the object. */
@@ -193,7 +182,6 @@ public class CharacterBase : MonoBehaviour {
 
         /* Call Collider Enter Functions */
         foreach (Vector2 exitContact in exitContacts) {
-            //if (!exitSet) {
             /* If contact exists (entries are zero in larger allocated ContactPoint2D[])*/
             if (exitContact != Vector2.zero) {
                 slopeAngleExit = Vector2.Angle(exitContact, Vector2.up);
@@ -202,22 +190,18 @@ public class CharacterBase : MonoBehaviour {
                 if (exitContact.x == 0) {
                     if (exitContact.y == 1) {
                         exitCollisionTypes.Add(CollisionType.Bot);
-                        exitSet = true;
                     }
                     else if (exitContact.y == -1) {
                         exitCollisionTypes.Add(CollisionType.Top);
-                        exitSet = true;
                     }
                 }
                 /* Horizontal Collision */
                 else if (slopeAngleExit > maxAngle) { //exitContact.y == 0
                     if (exitContact.x > 0) {
                         exitCollisionTypes.Add(CollisionType.Left);
-                        exitSet = true;
                     }
                     else if (exitContact.x < 0) {
                         exitCollisionTypes.Add(CollisionType.Right);
-                        exitSet = true;
                     }
                 }
                 /* Slope Collision */
@@ -225,38 +209,59 @@ public class CharacterBase : MonoBehaviour {
                     exitCollisionTypes.Add(CollisionType.Slope);
                 }
             }
-            //}
         }
     }
 
     /* Collision Methods: Custom ---------------------------------------------*/
-    // Enter
+    // Enter - Called immediately when changeState is called (before Main FixedUpdate).
     // Exit
-    // FixedUpdate
+    // FixedUpdate - Called after Main FixedUpdate
     // Collision Enter/Exit
+    // Input Events
     // Update
     // LateUpdate
     // Finally
 
-    void Falling_FixedUpdate() {
-        BaseFixedUpdate();
+    void Falling_Enter() {
+        Debug.Log("FALLING - Enter");
+    }
 
+    void Falling_FixedUpdate() {
+        Debug.Log("FALLING - Fixed Update");
         velocity.y += gravity * Time.deltaTime; // Apply Gravity until grounded
     }
 
     void Falling_OnCollisionEnter2D(Collision2D collision) {
         BaseCollisionEnter2D(collision);
         
-
-
-
         Debug.Log("FALLING - OnCollisionEnter");
-        velocity.y = 0;
-        fsm.ChangeState(States.Idle, StateTransition.Overwrite);
+        if(enterCollisionTypes.Count > 0) {
+            if (enterCollisionTypes.Contains(CollisionType.Bot)) {
+                velocity.y = 0;
+                enterCollisionTypes.Remove(CollisionType.Bot); // Addressed this collision so delete.
+                fsm.ChangeState(States.Idle, StateTransition.Overwrite);
+                // Continues execution from here after NextState.Enter() before FixedUpdate() next frame.
+            }
+        }
+        
     }
 
     void Idle_Enter() {
-        velocity.y = 0;
+        Debug.Log("IDLE - ENTER");
+    }
+
+    void Idle_FixedUpdate() {
+        Debug.Log("IDLE - FixedUpdate");
+    }
+
+
+    void Running_Enter() {
+        /* If Enter State and Collision has not been addressed. */
+        /*if (enterCollisionTypes.Count > 0 && enterCollisionTypes.Contains(CollisionType.Left)) {
+            velocity.x = 0;
+            enterCollisionTypes.Remove(CollisionType.Left); // Addressed this collision so delete.
+        }*/
+
     }
 
 }
