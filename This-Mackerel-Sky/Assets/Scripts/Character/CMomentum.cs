1962bb855fcb651cb09b7fc65dd31b875756ff9a
Kevin VanHorn <kevin.c.vanhor@gmail.com>
2018-01-20 06:28:45 -0600

    In - Progress - Momentum 01.

--- This-Mackerel-Sky/Assets/Scripts/Character/CMomentum.cs
+++ This-Mackerel-Sky/Assets/Scripts/Character/CMomentum.cs
@@ -2,6 +2,56 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* The live vars shared between individual momentem levels and CMomentum class. 
+ - can probably combine all 3 of these into 1. */
+public class MomentumGlobals
+{
+    public static float curMomentum;
+    public static float curMomentumLevel;
+}
+
+public class MomentumLevel : MonoBehaviour
+{
+    float drainDelayTime;
+    float maxStateMomentum;
+    bool lockedState = false; // cannot transition when locked.
+
+    public void Init(float maxMomentum, float drainDelayTime)
+    {
+        this.drainDelayTime = drainDelayTime;
+    }
+
+    IEnumerator WaitForDelay()
+    {
+        yield return null;
+    }
+
+    IEnumerator IncreaseMomentum()
+    {
+        
+        if(!lockedState && MomentumGlobals.curMomentum == maxStateMomentum)
+        {
+            // Fire Transition Event
+        }
+        yield return null;
+    }
+
+    public void OnDrainEvent()
+    {
+
+    }
+
+    public void SetMaxMomentum()
+    {
+        MomentumGlobals.curMomentum = maxStateMomentum;
+    }
+
+    public void ClearAllCoroutines()
+    {
+        
+    }
+}
+
 public class CMomentum : MonoBehaviour {
 
     /*
@@ -11,33 +61,28 @@ public class CMomentum : MonoBehaviour {
     What drains momentum?
     - staying at vel = 0
     */
-    private bool breakMomentumLevel = false; // Occurs on an event
+    public MomentumLevel[] momentumLevels;
+    private bool breakMomentum = false; // Occurs on an event
+    public float curMomentum = 0;
+    public float maxMomentum;
+    public float drainDelayTime;
 
-    struct MomemtumLevel{
-        float 
-        float drainDelayTime;
-        
+    // Use this for initialization
+    void Start () {
+        momentumLevels = new MomentumLevel[3]; // [5]
+        momentumLevels[0].Init(0, 0);
+        momentumLevels[1].Init(maxMomentum, drainDelayTime);
+        momentumLevels[2].Init(maxMomentum, drainDelayTime);
     }
 
-	// Use this for initialization
-	void Start () {
-	    	
-	}
+    public void OnBreakMomentum()
+    {
+        breakMomentum = true;
+    }
 
     IEnumerator CalcActiveSpeed()
     {
 
         yield return null;
-    }
-
-    IEnumerator OnWaitingAtZero()
-    {
-        /*for (float f = 0; f <= waitTime; f += 0.1f)
-        {
-            timeSinceAction = f;
-            yield return new WaitForSeconds(0.1f);
-        }
-        isWaiting = false;
-        yield return null;*/
     }
 }
