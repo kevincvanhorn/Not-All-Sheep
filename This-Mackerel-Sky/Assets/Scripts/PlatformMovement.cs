f7db5b2033ed8f0c7c4dc242fac4b17618c857d2
Kevin VanHorn <kevin.c.vanhor@gmail.com>
2018-01-23 10:29:33 -0600

    In-Progress: Pre-Moving Platforms 01.

---
+++ This-Mackerel-Sky/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    public bool isActive = true;
+    public float speed = 0;
+    public float moveDist;
+
+    private float waitTime = 0;
+    private float dir = 1;
+
+	// Use this for initialization
+	void Start () {
+        
+        StartCoroutine(SwitchDirection());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        waitTime = moveDist / speed;
+        transform.Translate(Vector2.up * speed * dir * Time.deltaTime);
+    }
+
+    IEnumerator SwitchDirection()
+    {
+        while (isActive)
+        {
+            yield return new WaitForSeconds(waitTime);
+            dir *= -1;
+        }
+    }
+}
