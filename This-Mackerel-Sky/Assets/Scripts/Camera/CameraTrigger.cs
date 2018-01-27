f69131ed25828945f31077b55707f502794739ee
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2018-01-27 12:46:29 -0600

    Added - Base Camera Zoom.

---
+++ This-Mackerel-Sky/Assets/Scripts/Camera/CameraTrigger.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Designed to interact with the CameraZoom class to request changes in the orthographic camera size.
+   - Calls Functions, does not modify the values directly. */
+public class CameraTrigger : MonoBehaviour {
+
+    public bool enterZoom;
+    public bool maintainZoom; // if false then set back when leave
+    public bool setDefault;
+
+    public CameraZoom camera; // **Set in Editor
+    private float prevSize;
+    public float zoomSize = 20;
+
+    private void Start()
+    {
+
+        setDefault = false;
+        zoomSize = 20;
+
+        if (!camera)
+        {
+            Debug.LogError("ERROR: No camera set/found.");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<CharacterBase>())
+        {
+            camera.RequestSizeChange(zoomSize);
+            //prevSize = camera.orthographicSize;
+            //camera.orthographicSize = zoomSize;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<CharacterBase>())
+        {
+            camera.SetDefaultSize();
+            //camera.orthographicSize = prevSize;
+        }
+    }
+}
---
+++ This-Mackerel-Sky/Assets/Scripts/Camera/CameraZoom.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Should be the sole class in charge of the main camera zoom/size.
+    - CameraTriggers request changes using the provided functions.
+    - Smoothing happens here (only!) using Coroutines. 
+    - Avoid using update b/c only triggers are starting events.*/
+public class CameraZoom : MonoBehaviour {
+
+    public float defaultSize;
+    private float curSize;
+    private float targetSize;
+
+    private Camera camera;
+
+    // Calculation Variables:
+    private float smoothVel;
+    public float smoothTime;
+    public float smoothSpeed;
+
+	// Use this for initialization
+	void Start () {
+        camera = GetComponent<Camera>();
+
+        /* Set Defaults*/
+        defaultSize = 14;
+        curSize = defaultSize;
+        targetSize = curSize;
+
+        smoothSpeed = 10;
+        smoothTime = 0.1f;
+
+        /* Set Camera*/
+        camera.orthographicSize = curSize;
+    }
+
+    // Immediately Sets the camera to a specific size.
+    public void SnapToSize(float snapSize)
+    {
+        camera.orthographicSize = snapSize;
+    }
+
+    public void SetDefaultSize()
+    {
+        RequestSizeChange(defaultSize);
+    }
+
+    public void RequestSizeChange(float targetSize)
+    {
+        this.targetSize = targetSize;
+        //smoothTime = Mathf.Abs(curSize - targetSize) / smoothSpeed ;
+        StartCoroutine(ChangeSizeSmooth());
+    }
+
+    IEnumerator ChangeSizeSmooth()
+    {
+        while (Mathf.Abs(curSize) <= Mathf.Abs(targetSize))
+        {
+            Debug.LogError("WHOAOHHH "+ targetSize);
+            Mathf.SmoothDamp(curSize, targetSize, ref smoothVel, smoothTime);
+            camera.orthographicSize = targetSize;
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return null;
+    }
+}
