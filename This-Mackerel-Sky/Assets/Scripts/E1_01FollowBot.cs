4f06de082f291b04fa4b908db8abdea4c72580c8
Kevin VanHorn <kevin.c.vanhorn@gmail.com>
2018-01-27 21:38:25 -0600

    In-Progress Enemy 01.

---
+++ This-Mackerel-Sky/Assets/Scripts/E1_01FollowBot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MonsterLove.StateMachine; // State-Machine Package.
+
+public class E1_01FollowBot : EnemyBase {
+
+    private StateMachine<E1_01FollowBotStates> fsm;
+
+    private enum E1_01FollowBotStates
+    {
+        Idle,
+        Fleeing,
+        Interest
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Idle_OnEnter()
+    {
+
+    }
+
+    void Idle_Update()
+    {
+
+    }
+}
