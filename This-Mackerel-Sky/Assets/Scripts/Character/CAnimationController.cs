cf35c6d60c9d386b1b621017c269d137091666ff
Kevin VanHorn <kevin.c.vanhor@gmail.com>
2018-01-28 09:46:32 -0600

    In-Progress: Animation States 01.

--- This-Mackerel-Sky/Assets/Scripts/Character/CAnimationController.cs
+++ This-Mackerel-Sky/Assets/Scripts/Character/CAnimationController.cs
@@ -6,7 +6,7 @@ public class CAnimationController : MonoBehaviour {
 
     private SpriteRenderer spriteRenderer;
     private CharacterBase character;
-
+    int charState;
     private Animator animator;
 
     // Use this for initialization
@@ -20,6 +20,8 @@ public class CAnimationController : MonoBehaviour {
 
     public void Update()
     {
+        charState = (int)character.fsm.State;
+
         bool flipSprite = (spriteRenderer.flipX ? (character.directionFacing == 1) : (character.directionFacing == -1));
         if (flipSprite)
         {
@@ -28,7 +30,8 @@ public class CAnimationController : MonoBehaviour {
 
         animator.SetBool("running", (Mathf.Abs(character.velocity.x) > 0) && (character.collisionState.Bot || character.collisionState.Slope));
         animator.SetBool("falling", character.collisionState.None);
-        animator.SetFloat("velocityX", (Mathf.Abs(character.velocity.x)));
+        //animator.SetFloat("velocityX", (Mathf.Abs(character.velocity.x)));
+        animator.SetFloat("velocityY", character.velocity.y);
         animator.SetBool("isGrounded", (character.collisionState.Bot || character.collisionState.Slope));
     }
 }
